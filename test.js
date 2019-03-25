3757dd2c7b97ffd0806987496c0e6f49009a96a7
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2019-03-24 21:24:00 -0700

    more testing after making repo public

--- test.js
+++ test.js
@@ -9,3 +9,11 @@
  * @body - [ ] Here's a checkbox? 
  * @body - [x] This one's done?
  */
+
+ /**
+ * @todo Verify (again) if new issues are added to project board automatically...
+ * @body Testing more for GitHub's todo app for checkboxes
+ * @body - [ ] Here's a checkbox? 
+ * @body - [x] This one's done?
+ * @body blahblah
+ */
