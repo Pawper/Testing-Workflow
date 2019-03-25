97ec6e9044fa9e1325ba02eeabd39f6701899b8f
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2019-03-24 22:03:58 -0700

    testing br and task lists

--- test.js
+++ test.js
@@ -7,3 +7,16 @@
  * @body - [x] This one's done?
  * @body blahblah
  */
+
+  /**
+ * @todo Verify if multiline works with BR & checkboxes
+ * @body Testing more for GitHub's todo app for checkboxes<br>- [ ] Here's a checkbox?<br>[x] This one's done<br>blahblah
+ */
+
+ /**
+ * @todo Verify if multiline works with BR & checkboxes & mutliple tags
+ * @body Testing more for GitHub's todo app for checkboxes<br>
+ * @body - [ ] Here's a checkbox?<br>
+ * @body - [x] This one's done<br>
+ * @body blahblah
+ */
