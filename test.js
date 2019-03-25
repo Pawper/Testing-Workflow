b5982a011e186b0c8209cb89069d0c5cbb70f28f
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2019-03-24 21:28:06 -0700

    testing removing to close

--- test.js
+++ test.js
@@ -1,14 +1,4 @@
-/**
- * @todo Verify todo checkboxes work - trying again
- * @body See how changes work to existing issues
- */
 
- /**
- * @todo Verify if new issues are added to project board automatically...
- * @body Testing more for GitHub's todo app for checkboxes
- * @body - [ ] Here's a checkbox? 
- * @body - [x] This one's done?
- */
 
  /**
  * @todo Verify (again) if new issues are added to project board automatically...
